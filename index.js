e3fbda779b9dd9c06e84732f7de2673298c4b33a
panddr <panddr@gmail.com>
2017-06-27 19:19:20 +0300

    wip

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var body = document.querySelector("body");
+
+  window.addEventListener("resize", handleResize);
+  function handleResize() {
+    var wWidth = window.innerWidth;
+    var wHeight = window.innerHeight;
+    if (wWidth/wHeight > 1.78) {
+      body.classList.remove("portrait");
+    } else {
+      body.classList.add("portrait");
+    }
+  }
+  handleResize();
+});
