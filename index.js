18ea081b96bb7c41e0fd2c3662d05a1990267828
panddr <panddr@gmail.com>
2017-06-30 14:22:28 +0300

    updates

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
   var body = document.querySelector("body");
+  var desktop = document.getElementById("desktop");
+  var mobile = document.getElementById("mobile");
+
+  if (window.innerWidth > 1024) {
+    mobile.remove();
+  } else {
+    desktop.remove();
+  }
 
   window.addEventListener("resize", handleResize);
   function handleResize() {
@@ -12,95 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
   handleResize();
-
-  // window.addEventListener("scroll", handleScroll);
-  function handleScroll() {
-    var text1 = document.getElementById("text-1");
-    var text2 = document.getElementById("text-2");
-    var text3 = document.getElementById("text-3");
-    var text4 = document.getElementById("text-4");
-    var text5 = document.getElementById("text-5");
-    var text6 = document.getElementById("text-6");
-    var text7 = document.getElementById("text-7");
-    var text8 = document.getElementById("text-8");
-    var video1 = document.getElementById("video-1");
-    var video2 = document.getElementById("video-2");
-    var video3 = document.getElementById("video-3");
-    var video4 = document.getElementById("video-4");
-    var video5 = document.getElementById("video-5");
-    var video6 = document.getElementById("video-6");
-    var video7 = document.getElementById("video-7");
-    console.log(body.scrollTop < text5.offsetTop)
-    if (body.scrollTop < text2.offsetTop) {
-      playVideo(video1);
-      pauseVideo(video2);
-      pauseVideo(video3);
-      pauseVideo(video4);
-      pauseVideo(video5);
-      video1.classList.remove("hide");
-      video2.classList.add("hide");
-      video3.classList.add("hide");
-      video4.classList.add("hide");
-      video5.classList.add("hide");
-    } else if (body.scrollTop < text3.offsetTop) {
-      pauseVideo(video1);
-      playVideo(video2);
-      pauseVideo(video3);
-      pauseVideo(video4);
-      pauseVideo(video5);
-      video1.classList.add("hide");
-      video2.classList.remove("hide");
-      video3.classList.add("hide");
-      video4.classList.add("hide");
-      video5.classList.add("hide");
-    } else if (body.scrollTop < text4.offsetTop) {
-      pauseVideo(video1);
-      pauseVideo(video2);
-      playVideo(video3);
-      pauseVideo(video4);
-      pauseVideo(video5);
-      video1.classList.add("hide");
-      video2.classList.add("hide");
-      video3.classList.remove("hide");
-      video4.classList.add("hide");
-      video5.classList.add("hide");
-    } else if (body.scrollTop < text5.offsetTop) {
-      pauseVideo(video1);
-      pauseVideo(video2);
-      pauseVideo(video3);
-      playVideo(video4);
-      pauseVideo(video5);
-      video1.classList.add("hide");
-      video2.classList.add("hide");
-      video3.classList.add("hide");
-      video4.classList.remove("hide");
-      video5.classList.add("hide");
-    } else if (body.scrollTop < text6.offsetTop) {
-      pauseVideo(video1);
-      pauseVideo(video2);
-      pauseVideo(video3);
-      pauseVideo(video4);
-      playVideo(video5);
-      video1.classList.add("hide");
-      video2.classList.add("hide");
-      video3.classList.add("hide");
-      video4.classList.add("hide");
-      video5.classList.remove("hide");
-    } else if (body.scrollTop < text7.offsetTop) {
-      pauseVideo(video1);
-      pauseVideo(video2);
-      pauseVideo(video3);
-      pauseVideo(video4);
-      playVideo(video5);
-      playVideo(video6);
-      playVideo(video7);
-      video1.classList.add("hide");
-      video2.classList.add("hide");
-      video3.classList.add("hide");
-      video4.classList.add("hide");
-      video5.classList.remove("hide");
-    }
-  }
 
   function playVideo(video) {
     if (video.paused) {
